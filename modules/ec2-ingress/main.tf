a9fd911a45fcd9c59fce1bc065c8dd3e280ab6ba
Amodamendis <amodarashmika2003@gmail.com>
2026-07-27 22:18:00 +0530

    make iam-roles & ec2-ingress

---
+++ modules/ec2-ingress/main.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "ubuntu_24_04" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "ingress_server" {
+  ami                  = data.aws_ami.ubuntu_24_04.id
+  instance_type        = "t2.large"
+  subnet_id            = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  iam_instance_profile = var.iam_instance_profile
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Ingress-Server"
+  }
+}
---
+++ modules/iam-roles/main.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "ec2_admin_role" {
+  name = "EC2AdministratorRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin_attach" {
+  role       = aws_iam_role.ec2_admin_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_admin_profile" {
+  name = "EC2AdministratorProfile"
+  role = aws_iam_role.ec2_admin_role.name
+}
