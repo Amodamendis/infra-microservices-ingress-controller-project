ef724e360389226fdb1a06200422dc37a3017899
Amodamendis <amodarashmika2003@gmail.com>
2026-07-28 23:50:55 +0530

    eks updated ith nodeprt support revert prev commit

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -72,3 +72,16 @@ resource "aws_eks_node_group" "main" {
     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+
+resource "aws_security_group_rule" "eks_nodeport_ingress" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  
+ 
+  security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  description       = "Allow Kubernetes NodePort traffic from the internet"
+}
--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -25,15 +25,3 @@ resource "aws_security_group" "ingress_sg" {
     Name = "ingress-server-sg"
   }
 }
-
-resource "aws_security_group_rule" "eks_nodeport_ingress" {
-  type              = "ingress"
-  from_port         = 30000
-  to_port           = 32767
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  
- 
-  security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-  description       = "Allow Kubernetes NodePort traffic from the internet"
-}
